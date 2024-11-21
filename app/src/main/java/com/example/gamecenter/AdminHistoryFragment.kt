cedd66ee3eb610a229d892a8d5bd3eb5a38be41a
BapakMu07 <alaziya03@gmail.com>
2024-11-21 07:40:13 +0700

    update

---
+++ app/src/main/java/com/example/gamecenter/AdminHistoryFragment.kt
@@ -0,0 +1,9 @@
+package com.example.gamecenter
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class AdminHistoryFragment : Fragment(R.layout.fragment_admin_history) {
+    // Implement fragment behavior here
+}
---
+++ app/src/main/java/com/example/gamecenter/AdminProfileFragment.kt
@@ -0,0 +1,8 @@
+package com.example.gamecenter
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class AdminProfileFragment : Fragment(R.layout.fragment_admin_profile) {
+    // Implement fragment behavior here
+}
